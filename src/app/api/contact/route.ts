fe5024d9c78a3e2c8b06999e5cb0e518d9480f60
Daham Abeyratne <Dahamabeyratney@gmail.com>
2025-12-28 11:49:55 +0530

    changed route.ts

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -1,4 +1,7 @@
 import {NextResponse} from "next/server";
+import {Resend} from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(req: Request){
     const {name, email, message } = await req.json();
@@ -10,9 +13,18 @@ export async function POST(req: Request){
         );
     }
 
-    console.log("RESEND_API_KEY exists:", !!process.env.RESEND_API_KEY);
-
-    console.log("New Contact message:", {name, email, message} );
+    await resend.emails.send({
+        from: "Portfolio <onboarding@resend.dev>",
+        to: ["dahamabeyratney@gmail.com"],
+        subject: `New Portfolio Message from ${name}`,
+        replyTo: email,
+        html: `
+        <p><strong>Name:</strong> ${name}</p>
+        <p><strong>Email:</strong> ${email}</p>
+        <p><strong>Message:</strong></p>
+        <p>${message}</p>
+        `,
+    });
 
     return NextResponse.json({success: true});
 }
